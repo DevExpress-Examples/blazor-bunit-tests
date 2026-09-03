78107c5f7baeac4ffaed7ac3658a8a41e2755e0d
Alexander Gusev <95075261+GusevAlexander-DevExpress@users.noreply.github.com>
2026-09-03 16:51:12 +0400

    refactored to use new bUnit syntax
    refactored to use new bUnit syntax

--- CS/DevExpressBunit.Test/GridApplyFilter.cs
+++ CS/DevExpressBunit.Test/GridApplyFilter.cs
@@ -1,32 +1,28 @@
 ﻿using Bunit;
 using DxTestProject.Pages;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 using Microsoft.Extensions.DependencyInjection;
 using DevExpress.Blazor;
 
 namespace DevExpressBunit.Test {
-    public class GridApplyFilter : IDisposable {
-        public BunitContext Context { get; }
+    public class GridApplyFilter : BunitContext {
         public GridApplyFilter() {
-            Context = new BunitContext();
-            Context.Services.AddOptions();
-            Context.AddDevExpressBlazorTesting();
+            Services.AddOptions();
+            this.AddDevExpressBlazorTesting();
         }
         [Fact]
-        public void CheckIfNodeClickAppliesFilter() {
-            var cut = Context.Render<Grid_ApplyFilter>();
+        public async Task CheckIfNodeClickAppliesFilter() {
+            var cut = Render<Grid_ApplyFilter>();
             var treeview = cut.FindComponent<DxTreeView>().Instance;
             var grid = cut.FindComponent<DxGrid>().Instance;
-            cut.InvokeAsync(() => treeview.SelectNode(x => x.Text == "Filter by date"));
+            await cut.InvokeAsync(() => treeview.SelectNode(x => x.Text == "Filter by date"));
             Assert.Equal(1, grid.GetVisibleRowCount());
-            cut.InvokeAsync(() => treeview.SelectNode(x => x.Text == "Filter by temperature"));
+            await cut.InvokeAsync(() => treeview.SelectNode(x => x.Text == "Filter by temperature"));
             Assert.Equal(3, grid.GetVisibleRowCount());
-            cut.InvokeAsync(() => treeview.SelectNode(x => x.Text == "Filter by precipitation"));
+            await cut.InvokeAsync(() => treeview.SelectNode(x => x.Text == "Filter by precipitation"));
             Assert.Equal(6, grid.GetVisibleRowCount());
-        }
-        public void Dispose() {
-            Context.Dispose();
         }
     }
 }
